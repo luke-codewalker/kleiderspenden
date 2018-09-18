3643b85c2bad2d32acdd741792ae73ef38084efc
Luke Codewalker <arathorn089@arcor.de>
2018-09-18 18:57:25 +0200

    backend api url depends on environment

--- src/globals.js
+++ src/globals.js
@@ -1,4 +1,7 @@
 const PLACE = "place";
 const MAP = "map";
-const API_ROOT = "https://kleiderspenden-api.herokuapp.com/api/sites";
+const API_ROOT =
+  process.env && process.env.NODE_ENV === "development"
+    ? "http://localhost:5000/api/sites"
+    : "https://kleiderspenden-api.herokuapp.com/api/sites";
 export { PLACE, MAP, API_ROOT };
